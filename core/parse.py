c7bbffab5258dd47ed704f03843df0863e82a3ac
Nicolas Spijkerman <nicospijke@gmail.com>
2025-05-29 10:38:22 +0900

    init project, and add lexer

---
+++ core/parse.py
@@ -0,0 +1,11 @@
+from core.lexer.lexer import Lexer, ScanState
+from core.error.error import Error
+
+
+def parse(buffer: str) -> str:
+    lexer = Lexer(buffer)
+    tokens = lexer.scan()
+    if lexer.scan_state == ScanState.FAILURE:
+        return Error.build_lexer_error_msg(lexer)
+    print(tokens)
+    return ""
---
+++ main.py
@@ -0,0 +1,26 @@
+from core.parse import parse
+import traceback
+import sys
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <token_file>")
+        sys.exit(1)
+    input_file: str = sys.argv[1]
+    try:
+        with open(input_file, 'r') as file:
+            buffer = file.read()
+        result = parse(buffer)
+        print(result)
+    except FileNotFoundError:
+        print(f"error: File '{input_file}' not found.")
+        sys.exit(1)
+    except Exception as err:
+        print(f"error: {err}")
+        traceback.print_exc()
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
